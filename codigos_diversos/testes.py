3739e2238141bea05fc4f55c84c3cef7370e7810
Carla Edila Silveira <rosa.carla@pucpr.edu.br>
2024-11-01 19:15:51 -0300

    Update testes.py

--- codigos_diversos/testes.py
+++ codigos_diversos/testes.py
@@ -1,2 +1,9 @@
+# Primieira versão tinha erro 
+#nome_todo = str("Carla Edila Santos da Rosa Silveira")
+#nome_todo.group(1)
+# Saida: AttributeError: 'str' object has no attribute 'group'
+
+# Segunda versão corrigida
 nome_todo = str("Carla Edila Santos da Rosa Silveira")
-nome_todo.group(1)
+primeiro_nome = nome_todo.split()[0]
+print(primeiro_nome)  # Saída: Carla
