e558abe17d67619c6b71db640f831431b564d434
Carla Edila Silveira <rosa.carla@pucpr.edu.br>
2024-11-01 19:47:46 -0300

    Update Turtle.py

--- codigos_diversos/Turtle.py
+++ codigos_diversos/Turtle.py
@@ -1,30 +1,24 @@
-#Tartaruga na tela
+# Tartaruga na tela - versão atualizada
 import random
 import turtle
 
-def isInScreen(w,t):
-    leftBound = - w.window_width()/2
-    rightBound = w.window_width()/2
-    topBound = w.window_height()/2
-    bottomBound = -w.window_height()/2
-    
-    turtleX = t.xcor()
-    turtleY = t.ycor()
+def isInScreen(wn, t):
+    left_bound = -wn.window_width() / 2
+    right_bound = wn.window_width() / 2
+    top_bound = wn.window_height() / 2
+    bottom_bound = -wn.window_height() / 2
 
-    stillIn = True
-    if turtleX > rightBound or turtleX < leftBound:
-        stillIn = False
-    if turtleY > topBound or turtleY < bottomBound:
-        stillIn = False
+    turtle_x = t.xcor()
+    turtle_y = t.ycor()
 
-    return stillIn
+    return left_bound < turtle_x < right_bound and bottom_bound < turtle_y < top_bound
 
 t = turtle.Turtle()
 wn = turtle.Screen()
 
 t.shape('turtle')
-while isInScreen(wn,t):
-    coin = random.randrange(0,2)
+while isInScreen(wn, t):
+    coin = random.randrange(0, 2)
     if coin == 0:
         t.left(90)
     else:
